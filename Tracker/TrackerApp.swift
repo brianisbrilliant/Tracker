5efe5e91613ac242a797a0dda91b471123f6b640
Brian Foster <bfoster6@butlercc.edu>
2021-05-05 08:08:15 -0500

    Initial Commit

---
+++ Tracker/TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackerApp.swift
+//  Tracker
+//
+//  Created by Professor Foster on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
